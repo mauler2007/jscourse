7d60ad2f0d969c0107d45f0daa322cdc7efdef1d
Your Namemauler2007 <mauler2007@gmail.com>
2021-10-04 19:00:51 +0300

    added 2 hw

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('123');
+'use strict'
+
+let title= 'projectName',
+  screens= 'easy,  hard,  interactive',
+  screenPrice= 1000,
+  rollback= 100,
+  fullPrice= 5000,
+  adaptive= true;
+
+console.log('тип данных: ' + typeof(title));
+console.log('тип данных: ' + typeof(fullPrice));
+console.log('тип данных: ' + typeof(adaptive));
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+
+console.log('Стоимость разработки сайта ' + fullPrice+  ' рублей');
+
+let newArr = screens.split(', ');
+console.log(newArr);
+
+console.log('Процент отката посреднику за работу: ' + (fullPrice * (rollback / 100)));
