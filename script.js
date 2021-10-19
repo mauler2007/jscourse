cb3dad99bdb966d28be370ced42e542758f1acd4
Your Namemauler2007 <mauler2007@gmail.com>
2021-10-19 19:37:44 +0300

    add lesson6

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('123');
+
+let answer = 10;
+
+let isNumber = function (num) {
+  return !isNaN(parseFloat(num) && isFinite(num));
+}
+
+function gameStart() {
+
+  let question = prompt('Угадай число от 1 до 100', '10');
+  
+  if (question === null) {
+    alert('Игра окончена');
+    return;
+  } else if (answer < question) {
+    alert('Загаданное число меньше');
+    gameStart();
+  } else if (answer > question) {
+    alert('Загаданное число больше');
+    gameStart();
+  } else if (answer == question) {
+    alert('Поздравляю, Вы угадали!!!');
+    return;
+  } else if (!isNumber(question)) {
+    alert('Введи число!');
+    gameStart();
+  } else {
+    alert('Введи число!');
+    gameStart();
+  }
+}
+
+gameStart();
