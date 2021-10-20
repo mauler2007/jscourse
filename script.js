2524f53376bc6cc129fc34eabf20de47b25e9423
Your Namemauler2007 <mauler2007@gmail.com>
2021-10-20 08:53:19 +0300

    aply lock

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-
-let answer = 10;
+'use strict'
 
 let isNumber = function (num) {
   return !isNaN(parseFloat(num) && isFinite(num));
-}
+};
 
-function gameStart() {
-
-  let question = prompt('Угадай число от 1 до 100', '10');
+// Принимаю параметр  который буду использовать в дочерней функции
+function start(num) {
   
-  if (question === null) {
-    alert('Игра окончена');
-    return;
-  } else if (answer < question) {
-    alert('Загаданное число меньше');
-    gameStart();
-  } else if (answer > question) {
-    alert('Загаданное число больше');
-    gameStart();
-  } else if (answer == question) {
-    alert('Поздравляю, Вы угадали!!!');
-    return;
-  } else if (!isNumber(question)) {
-    alert('Введи число!');
-    gameStart();
-  } else {
-    alert('Введи число!');
-    gameStart();
+  let answer = num;
+
+  function compare() {
+
+    let question = prompt('Угадай число от 1 до 100', '10');
+
+    if (question === null) {
+      alert('Игра окончена');
+      return;
+    } else if (answer < question) {
+      alert('Загаданное число меньше');
+      compare();
+    } else if (answer > question) {
+      alert('Загаданное число больше');
+      compare();
+    } else if (answer == question) {
+      alert('Поздравляю, Вы угадали!!!');
+      return;
+    } else if (!isNumber(question)) {
+      alert('Введи число!');
+      compare();
+    } else {
+      alert('Введи число!');
+      compare();
+    }
   }
+  compare();
 }
 
-gameStart();
+start(10);
