4514e8a3519f49f91930d1f646f3e0075996160c
Your Namemauler2007 <mauler2007@gmail.com>
2021-11-08 23:09:23 +0200

    total lesson

--- js11/script.js
+++ js11/script.js
@@ -8,19 +8,34 @@ const button = document.getElementById('btn'),
   e_btn = document.getElementById('e_btn');
 
 
-button.addEventListener('click' , function() {
+button.addEventListener('click', function () {
   square.style.backgroundColor = textInput.value;
-  e_btn.style.display='none';
+  e_btn.style.display = 'none';
 });
 
-range.addEventListener('input', function() {
-  // textInput.setAttribute('min' , 100);
-  // textInput.setAttribute('max' , 100);
+// const logger = function (event) {
+//   console.log(event.target.value);
+// };
 
-  // circle.style.width = textInput.value;
-  console.log(textInput.value);
-});
+const log = function (event) {
+  let val = event.target.value;
+  circle.style.width = `${val}%`,
+    circle.style.height = `${val}%`;
+  console.log(val);
+};
 
-console.dir(circle);
+range.addEventListener('input', log);
 
 
+// range.addEventListener('change', log);
+// range.addEventListener('focus', logger);
+// range.addEventListener('blur', logger);
+
+// textInput.setAttribute('min', 100);
+// textInput.setAttribute('max', 100);
+
+// circle.style.width = changeWidth;
+// console.log(textInput.value);
+
+
+// console.dir(circle);
